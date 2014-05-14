6ec013dcea6ec95e6232632b0f403dfd26390f6c
John Watson <john@photodojo.org>
2014-05-14 09:14:20 -0400

    Bugfixes.

--- app/assets/javascripts/window.js
+++ app/assets/javascripts/window.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 
-  L.mapbox.map('map', 'jwatson.i4bh1ae1');
+  var map = L.mapbox.map('map', 'jwatson.i4bh1ae1').setView([42.35872, -71.16568], 15);
+  L.mapbox.featureLayer({
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-71.16568, 42.35872],
+    },
+    properties: {
+      title: 'Harry Parker Boathouse',
+      description: '20 Nonantum Rd. Brighton, MA',
+      'marker-size': 'large',
+      'marker-color': '#77c'
+    }
+  }).addTo(map);
 
   // Smooth scroll to the anchor target in the nav bar.
   $('header nav a[href *= #]').each(function scroll() {
